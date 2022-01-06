13c9ec5ede2385bd7ab58ccac63807fdaa148cec
VictorGNetto <victornetto@discente.ufg.br>
2022-01-06 09:29:09 -0300

    implementação do DFA que reconhece os tokens; refatoração da função scan

--- src/lexical_automaton.rs
+++ src/lexical_automaton.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub enum AutomatonState {
     Initial,         // 0
     Accept(u8),      // 1, 2, ..., 18
@@ -5,10 +6,18 @@ pub enum AutomatonState {
     Error,           // lexical error
 }
 
+pub enum Action {
+    None,
+    GoBack,
+    UpdateLexeme,
+    ClearLexeme,
+    ShowError,
+}
+
 pub struct Automaton {
     pub state: AutomatonState,
     pub done: bool,
-    pub go_back: bool,
+    pub action: Action,
 }
 
 impl Automaton {
@@ -16,87 +25,215 @@ impl Automaton {
         Automaton {
             state: AutomatonState::Initial,
             done: false,
-            go_back: false,
+            action: Action::None,
         }
     }
 
     pub fn advance(&mut self, c: char) {
         match self.state {
-            AutomatonState::Initial => match c {
-                '0'..='9' => {
-                    self.state = AutomatonState::Accept(1);
-                    return;
+            AutomatonState::Initial => {
+                match c {
+                    '0'..='9' => self.go_to_state(AutomatonState::Accept(1), Action::UpdateLexeme),
+                    '"' => self.go_to_state(AutomatonState::NonAccept('d'), Action::UpdateLexeme),
+                    'a'..='z' | 'A'..='Z' => {
+                        self.go_to_state(AutomatonState::Accept(5), Action::UpdateLexeme);
+                    }
+                    '{' => self.go_to_state(AutomatonState::NonAccept('e'), Action::None),
+                    '<' => self.go_to_state(AutomatonState::Accept(8), Action::UpdateLexeme),
+                    '>' => self.go_to_state(AutomatonState::Accept(12), Action::UpdateLexeme),
+                    '=' => self.go_to_state(AutomatonState::Accept(14), Action::UpdateLexeme),
+                    '+' | '-' | '*' | '/' => {
+                        self.done = true;
+                        self.go_to_state(AutomatonState::Accept(15), Action::UpdateLexeme)
+                    }
+                    '(' => {
+                        self.done = true;
+                        self.go_to_state(AutomatonState::Accept(16), Action::UpdateLexeme);
+                    }
+                    ')' => {
+                        self.done = true;
+                        self.go_to_state(AutomatonState::Accept(17), Action::UpdateLexeme);
+                    }
+                    ';' => {
+                        self.done = true;
+                        self.go_to_state(AutomatonState::Accept(18), Action::UpdateLexeme);
+                    }
+                    '\n' | '\r' | ' ' => self.go_to_state(AutomatonState::Initial, Action::None),
+                    _ => self.error(),
                 }
-                '"' => {
-                    self.state = AutomatonState::NonAccept('d');
-                    return;
+                return;
+            }
+            AutomatonState::Accept(1) => {
+                match c {
+                    '0'..='9' => self.go_to_state(AutomatonState::Accept(1), Action::UpdateLexeme),
+                    '.' => self.go_to_state(AutomatonState::NonAccept('a'), Action::UpdateLexeme),
+                    'e' | 'E' => {
+                        self.go_to_state(AutomatonState::NonAccept('b'), Action::UpdateLexeme);
+                    }
+                    c if is_in_alphabet(c) => {
+                        self.done = true;
+                        self.action = Action::GoBack;
+                    }
+                    _ => self.error(),
                 }
-                'a'..='z' | 'A'..='Z' => {
-                    self.state = AutomatonState::Accept(5);
-                    return;
+                return;
+            }
+            AutomatonState::Accept(2) => {
+                match c {
+                    '0'..='9' => self.go_to_state(AutomatonState::Accept(2), Action::UpdateLexeme),
+                    'e' | 'E' => {
+                        self.go_to_state(AutomatonState::NonAccept('b'), Action::UpdateLexeme);
+                    }
+                    c if is_in_alphabet(c) => {
+                        self.done = true;
+                        self.action = Action::GoBack;
+                    }
+                    _ => self.error(),
                 }
-                '{' => {
-                    self.state = AutomatonState::NonAccept('e');
-                    return;
+                return;
+            }
+            AutomatonState::Accept(3) => {
+                match c {
+                    '0'..='9' => self.go_to_state(AutomatonState::Accept(3), Action::UpdateLexeme),
+                    c if is_in_alphabet(c) => {
+                        self.done = true;
+                        self.action = Action::GoBack;
+                    }
+                    _ => self.error(),
                 }
-                '<' => {
-                    self.state = AutomatonState::Accept(8);
-                    return;
+                return;
+            }
+            AutomatonState::Accept(5) => {
+                match c {
+                    '0'..='9' => self.go_to_state(AutomatonState::Accept(5), Action::UpdateLexeme),
+                    'a'..='z' | 'A'..='Z' => {
+                        self.go_to_state(AutomatonState::Accept(5), Action::UpdateLexeme);
+                    }
+                    '_' => self.go_to_state(AutomatonState::Accept(5), Action::UpdateLexeme),
+                    c if is_in_alphabet(c) => {
+                        self.done = true;
+                        self.action = Action::GoBack;
+                    }
+                    _ => self.error(),
                 }
-                '>' => {
-                    self.state = AutomatonState::Accept(12);
-                    return;
+                return;
+            }
+            AutomatonState::Accept(8) => {
+                match c {
+                    '=' => {
+                        self.done = true;
+                        self.go_to_state(AutomatonState::Accept(9), Action::UpdateLexeme);
+                    }
+                    '>' => {
+                        self.done = true;
+                        self.go_to_state(AutomatonState::Accept(10), Action::UpdateLexeme);
+                    }
+                    '-' => {
+                        self.done = true;
+                        self.go_to_state(AutomatonState::Accept(11), Action::UpdateLexeme);
+                    }
+                    c if is_in_alphabet(c) => {
+                        self.done = true;
+                        self.action = Action::GoBack;
+                    }
+                    _ => self.error(),
                 }
-                '=' => {
-                    self.state = AutomatonState::Accept(14);
-                    return;
+                return;
+            }
+            AutomatonState::Accept(12) => {
+                match c {
+                    '=' => {
+                        self.done = true;
+                        self.go_to_state(AutomatonState::Accept(13), Action::UpdateLexeme);
+                    }
+                    c if is_in_alphabet(c) => {
+                        self.done = true;
+                        self.action = Action::GoBack;
+                    }
+                    _ => self.error(),
                 }
-                '+' | '-' | '*' | '/' => {
-                    self.state = AutomatonState::Accept(15);
-                    return;
+                return;
+            }
+            AutomatonState::NonAccept('a') => {
+                match c {
+                    '0'..='9' => self.go_to_state(AutomatonState::Accept(2), Action::UpdateLexeme),
+                    _ => self.error(),
                 }
-                '(' => {
-                    self.state = AutomatonState::Accept(16);
-                    return;
+                return;
+            }
+            AutomatonState::NonAccept('b') => {
+                match c {
+                    '0'..='9' => self.go_to_state(AutomatonState::Accept(3), Action::UpdateLexeme),
+                    '+' | '-' => {
+                        self.go_to_state(AutomatonState::NonAccept('c'), Action::UpdateLexeme);
+                    }
+                    _ => self.error(),
                 }
-                ')' => {
-                    self.state = AutomatonState::Accept(16);
-                    return;
+                return;
+            }
+            AutomatonState::NonAccept('c') => {
+                match c {
+                    '0'..='9' => self.go_to_state(AutomatonState::Accept(3), Action::UpdateLexeme),
+                    _ => self.error(),
                 }
-                ';' => {
-                    self.state = AutomatonState::Accept(16);
-                    return;
+                return;
+            }
+            AutomatonState::NonAccept('d') => {
+                match c {
+                    '"' => {
+                        self.done = true;
+                        self.go_to_state(AutomatonState::Accept(4), Action::UpdateLexeme);
+                    }
+                    c if is_in_alphabet(c) => {
+                        self.go_to_state(AutomatonState::NonAccept('d'), Action::UpdateLexeme);
+                    }
+                    _ => self.error(),
                 }
-                '\n' | '\r' | ' ' => return,
-                _ => {
-                    self.state = AutomatonState::Error;
-                    return;
+                return;
+            }
+            AutomatonState::NonAccept('e') => {
+                match c {
+                    '}' => self.go_to_state(AutomatonState::Initial, Action::None),
+                    c if is_in_alphabet(c) => {
+                        self.go_to_state(AutomatonState::NonAccept('e'), Action::None);
+                    }
+                    _ => self.error(),
                 }
-            },
-            AutomatonState::Accept(1) => {}
-            AutomatonState::Accept(2) => {}
-            AutomatonState::Accept(3) => {}
-            AutomatonState::Accept(4) => {}
-            AutomatonState::Accept(5) => {}
-            AutomatonState::Accept(6) => {}
-            AutomatonState::Accept(7) => {}
-            AutomatonState::Accept(8) => {}
-            AutomatonState::Accept(9) => {}
-            AutomatonState::Accept(10) => {}
-            AutomatonState::Accept(11) => {}
-            AutomatonState::Accept(12) => {}
-            AutomatonState::Accept(13) => {}
-            AutomatonState::Accept(14) => {}
-            AutomatonState::Accept(15) => {}
-            AutomatonState::Accept(16) => {}
-            AutomatonState::Accept(17) => {}
-            AutomatonState::Accept(18) => {}
-            AutomatonState::NonAccept('a') => {}
-            AutomatonState::NonAccept('b') => {}
-            AutomatonState::NonAccept('c') => {}
-            AutomatonState::NonAccept('d') => {}
-            AutomatonState::NonAccept('e') => {}
+                return;
+            }
             _ => (),
         }
+    }
+
+    // Puts the automaton in the AutomatonState::Error state and
+    // ends its execution by setting self.done to true
+    fn error(&mut self) {
+        self.state = AutomatonState::Error;
+        self.done = true;
+        self.action = Action::ShowError;
+    }
+
+    // Puts the automaton in a new state and set a new action to be performed
+    fn go_to_state(&mut self, state: AutomatonState, action: Action) {
+        self.state = state;
+        self.action = action;
+    }
+}
+
+fn is_in_alphabet(c: char) -> bool {
+    match c {
+        '0'..='9' => true,
+        'a'..='z' => true,
+        'A'..='Z' => true,
+        ',' | '.' | ';' | ':' => true,
+        '<' | '>' | '=' => true,
+        '(' | ')' => true,
+        '[' | ']' => true,
+        '{' | '}' => true,
+        '+' | '-' | '*' | '/' => true,
+        '!' | '?' | '\\'  => true,
+        '"' | '\'' => true,
+        '\n' | '\r' | ' ' => true,
+        _ => false,
     }
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -25,23 +25,28 @@ impl Scanner {
 
         while let Some(c) = self.read_char() {
             automaton.advance(c);
+            // println!("{:?}", automaton.state);
 
-            if automaton.go_back {
-                self.put_back();
-            } else {
-                lexeme.push(c);
+            match automaton.action {
+                Action::GoBack => self.put_back(),
+                Action::UpdateLexeme => lexeme.push(c),
+                Action::ClearLexeme => lexeme.clear(),
+                Action::ShowError => {
+                    println!("Error sintático na linha 4, coluna {}: {}", self.cursor, c);
+                },
+                Action::None => (),
             }
 
             if automaton.done {
-                return self.build_token(lexeme);
+                return self.build_token(lexeme, automaton.state);
             }
         }
 
-        return Token {
+        Token {
             class: String::from("EOF"),
             lexeme: String::from("EOF"),
             tk_type: None,
-        };
+        }
     }
 
     // return a char by consuming the internal BufReader file
@@ -69,11 +74,95 @@ impl Scanner {
     }
 
     // A Token fabric
-    fn build_token(&mut self, lexeme: String) -> Token {
-        return Token {
-            class: String::from("..."),
-            lexeme: lexeme,
-            tk_type: None,
-        };
+    fn build_token(&mut self, lexeme: String, automaton_state: AutomatonState) -> Token {
+        // println!(">>> {:?}", automaton_state);
+
+        match automaton_state {
+            AutomatonState::Accept(1) => Token {
+                class: String::from("Num"),
+                lexeme,
+                tk_type: Some(String::from("inteiro")),
+            },
+            AutomatonState::Accept(2) | AutomatonState::Accept(3) => Token {
+                class: String::from("Num"),
+                lexeme,
+                tk_type: Some(String::from("real")),
+            },
+            AutomatonState::Accept(4) => Token {
+                class: String::from("Lit"),
+                lexeme,
+                tk_type: Some(String::from("literal")),
+            },
+            AutomatonState::Accept(5) => Token {
+                class: String::from("id"),
+                lexeme,
+                tk_type: None,
+            },
+            AutomatonState::Accept(8) => Token {
+                class: String::from("OPR"),
+                lexeme,
+                tk_type: None,
+            },
+            AutomatonState::Accept(9) => Token {
+                class: String::from("OPR"),
+                lexeme,
+                tk_type: None,
+            },
+            AutomatonState::Accept(10) => Token {
+                class: String::from("OPR"),
+                lexeme,
+                tk_type: None,
+            },
+            AutomatonState::Accept(11) => Token {
+                class: String::from("RCB"),
+                lexeme,
+                tk_type: None,
+            },
+            AutomatonState::Accept(12) => Token {
+                class: String::from("OPR"),
+                lexeme,
+                tk_type: None,
+            },
+            AutomatonState::Accept(13) => Token {
+                class: String::from("OPR"),
+                lexeme,
+                tk_type: None,
+            },
+            AutomatonState::Accept(14) => Token {
+                class: String::from("OPR"),
+                lexeme,
+                tk_type: None,
+            },
+            AutomatonState::Accept(15) => Token {
+                class: String::from("OPM"),
+                lexeme,
+                tk_type: None,
+            },
+            AutomatonState::Accept(16) => Token {
+                class: String::from("AB_P"),
+                lexeme,
+                tk_type: None,
+            },
+            AutomatonState::Accept(17) => Token {
+                class: String::from("FC_P"),
+                lexeme,
+                tk_type: None,
+            },
+            AutomatonState::Accept(18) => Token {
+                class: String::from("PT_V"),
+                lexeme,
+                tk_type: None,
+            },
+            AutomatonState::Error => Token {
+                class: String::from("ERROR"),
+                lexeme: String::from("NULO"),
+                tk_type: None,
+            },
+            _ => Token {
+                class: String::from(""),
+                lexeme: lexeme,
+                tk_type: None,
+            },
+        }
     }
 }
